ce8b8e52969ed190364431c06de7fbf7096fd750
Chen_Fuqiang_12138 <chanfuqiang@hotmail.com>
2020-06-16 20:35:38 +0800

    complete the pratise of the statistics for the words' num of one String

--- code/necessary_practise/calculate_the_counts_num_in_one_string/exercise03.py
+++ code/necessary_practise/calculate_the_counts_num_in_one_string/exercise03.py
@@ -1,5 +1,14 @@
 """
 随即输入一个字符串，并统计出每个字符出现的次数
+比如aidofjs
 """
+str_list = {}
 str_random = input("请输入一个字符串：")
-for item in 
+for item in str_random:
+    if item in str_list:
+        count = 1
+        count += 1
+    elif item not in str_list:
+        count = 1
+    str_list[item]=count
+print(str_list)

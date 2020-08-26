e3c336f958e01ba2128544936f23291493c3f41d
Chen_Fuqiang_12138 <chanfuqiang@hotmail.com>
2020-08-27 00:22:17 +0800

    knowledge on the real parameter.

--- code/basic_knowledge/parameter/formal_parameter.py
+++ code/basic_knowledge/parameter/formal_parameter.py
@@ -15,6 +15,43 @@ fun01(c=2,b=3)
 
 #练习：定义函数，根据小时 分钟 秒 计算总秒数，要求可以灵活计算秒数
 def time_calculator(hour = 0, minute = 0, second=0):
-    second_total = hour * 3600 + minute * 60 + second
-    return second_total
+    return hour * 3600 + minute * 60 + second
 print(time_calculator(hour=1,second=10))
+
+# 2. 位置形参
+def fun02(a, b, c, d):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+
+# 3.星号元组形参: * 将所有实参合并为一个元组
+# 作用：让实参个数无限
+def fun03(*args):#arg--argument形参的意思，一般这个地方要写成args这样
+    print(args)
+
+#练习：定义数值相加的函数
+def sum_num(*args):
+    sum = 0
+    for item in args:
+        sum += item
+    print(sum)
+sum_num(2,3,567,34)
+
+# 4.命名关键字形参:在星号元组形参以后的位置形参
+# 目的：要求实参必须使用关键字实参.
+def fun04(a, *args, b):#这种方式a,b至少是要有值的
+    print(a)
+    print(args)
+    print(b)
+fun04(1, b=2)
+fun04(1, 2, 3, 4, b=2)
+
+#或
+def fun05(*, a, b):
+    print(a)
+    print(b)
+fun05(a=1, b=2)
+
+# 5. 双星号字典形参：**目的是将实参合并为字典.
+#               实参可以传递数量无限的关键字实参.

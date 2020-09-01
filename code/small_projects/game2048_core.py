7010935c92d0cee154bfacdb35d02bc110a12719
Chen_Fuqiang_12138 <chanfuqiang@hotmail.com>
2020-09-01 10:42:40 +0800

    2048 game core.

---
+++ code/small_projects/game2048_core.py
@@ -0,0 +1,16 @@
+"""
+2048  游戏核心算法
+"""
+# 练习１：零元素移至末尾
+# 　　[2,0,2,0]  --> [2,2,0,0]
+#    [2,0,0,2]  --> [2,2,0,0]
+#    [2,4,0,2]  --> [2,4,2,0]
+def move_zero_bottom(listA):
+    for c in range(len(listA)):
+        for i in range(-1, -len(listA)):
+            if listA[c] == 0 and listA[i] != 0:
+                listA[c], listA[i] = listA[i], listA[c]
+                break
+list01=[2,0,2,0]
+move_zero_bottom(list01)
+print(list01)

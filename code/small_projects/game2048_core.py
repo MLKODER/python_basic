c6ada6998f2c9318b10b1cb3a4ca1ecb6905e634
Chen_Fuqiang_12138 <chanfuqiang@hotmail.com>
2020-09-07 00:02:17 +0800

    2048 game core update practise 2.

--- code/small_projects/game2048_core.py
+++ code/small_projects/game2048_core.py
@@ -29,3 +29,42 @@ def zero_to_end(listB):
 list02=[0,2,4,0,5,0,0,2,0,6]
 zero_to_end(list02)
 print(list02)
+
+
+# 练习2：将相同数字合并（只合并一次）
+# 　　[2,2,0,0]  --> [4,0,0,0]
+#    [2,0,0,2]  --> [4,0,0,0]
+#    [2,0,4,0]  --> [2,4,0,0]
+#    [2,2,2,2]  --> [4,4,0,0]
+#    [2,2,2,0]  --> [4,2,0,0]
+
+#思想一：先遍历看有没有相同的，然后移动0
+def combine_and_moveZero(listC):
+    for i in range(len(listC)):
+        for x in range(i+1, len(listC)):
+            if listC[i] == listC[x]:
+                listC[i] += listC[x]
+                listC[x] = 0
+                break
+    move_back_zero(listC)
+def move_back_zero(listD):
+    for i in range(len(listD)):
+        for x in range(i+1, len(listD)):
+            if listD[i] == 0 and listD [x] != 0:
+                listD[i], listD[x] = listD[x],listD[i]
+                break
+list03=[2,2,2,0]
+combine_and_moveZero(list03)
+print(list03)
+
+#思想二：先移动0，后累加相同的数字
+def move_to_back(listE):
+    zero_to_end(listE)
+    for i in range(len(listE)-1):
+        if listE[i] == listE[i+1]:
+            listE[i] += listE[i+1]
+            del listE[i+1]
+            listE.append(0)
+list04 = [2,2,2,0]
+move_to_back(list04)
+print(list04)
